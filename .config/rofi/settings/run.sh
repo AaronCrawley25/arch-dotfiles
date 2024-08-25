90588147c237e20c32e3a7c753c548c90cfef027
Aaron Crawley <aaron.crawley@outlook.com.au>
2024-08-25 23:31:02 +0800

    More changes

--- .config/rofi/settings/run.sh
+++ .config/rofi/settings/run.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/zsh
 
 DND=$([ $(dunstctl is-paused) = 'true' ] && echo '󰂛' || echo '󰂚')
-selected_option=$(echo "󰤥 Wifi\n󰂯 Bluetooth\n󰍹 Display\n$DND Do Not Disturb\n Audio\n⏻ Power" | rofi -dmenu -i -theme ~/.config/rofi/settings/settings.rasi -p "Settings")
+selected_option=$(echo "󰤥 Wifi\n󰂯 Bluetooth\n󰍹 Display\n$DND Do Not Disturb\n Audio\n⏻ Power\n󰐪 Printers" | rofi -dmenu -i -theme ~/.config/rofi/settings/settings.rasi -p "Settings")
 [ $selected_option = "󰤥 Wifi" ] && networkmanager_dmenu
 [ $selected_option = "󰂯 Bluetooth" ] && rofi-bluetooth
 [ $selected_option = "󰍹 Display" ] && nwg-displays
 [ $selected_option = "$DND Do Not Disturb" ] && dunstctl set-paused toggle && pkill -RTMIN+8 waybar
 [ $selected_option = " Audio" ] && ~/.config/rofi/settings/run_audio.py
 [ $selected_option = "⏻ Power" ] && ~/.config/rofi/settings/run_power.sh
+[ $selected_option = "󰐪 Printers" ] && xdg-open http://localhost:631
